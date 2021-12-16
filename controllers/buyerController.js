b92fc75bcdb023c6a0802029103d95ffcbc49f21
arumdyaharum <arumdyaharum@gmail.com>
2021-12-16 08:59:29 +0700

    added validation on daftar

--- controllers/buyerController.js
+++ controllers/buyerController.js
@@ -3,25 +3,37 @@ const bcryptjs = require('bcryptjs')
 
 class Controller {
   static buyerDaftar(req, res) {
-    res.render('buyer/buyerDaftar')
+    let errors = []
+    if(req.query.errors) {
+      errors = req.query.errors.split(',')
+    }
+    res.render('buyer/buyerDaftar', {errors})
   }
 
   static buyerDaftarPost(req, res) {
     const { name, email, password, gender, age } = req.body
-    const valueUsers = { name, email, password, role: 'buyer', createdAt: new Date(), updatedAt: new Date() }
-    Users.create(valueUsers)
-      .then(data => {
-        const valueBuyers = { gender, age, BuyerId: data.id, createdAt: new Date(), updatedAt: new Date() }
-        return Buyers.create(valueBuyers)
-      })
-      .then(data => res.redirect('/buyer/login'))
-      .catch(err => {
-        if(err.name == "SequelizeValidationError") {
-          res.send(err.errors.map(el => el.message))
-        } else {
-          res.send(err)
-        }
-      })
+    Users.findOne({where: {email: email}})
+    .then(data => {
+      if(data) {
+        let errors = 'Email sudah ada. Harap gunakan email yang lain.'
+        return res.redirect(`/buyer/daftar?errors=${errors}`)
+      }
+      const valueUsers = { name, email, password, role: 'buyer', createdAt: new Date(), updatedAt: new Date() }
+      return Users.create(valueUsers)
+    })
+    .then(data => {
+      const valueBuyers = { gender, age, BuyerId: data.id, createdAt: new Date(), updatedAt: new Date() }
+      return Buyers.create(valueBuyers)
+    })
+    .then(data => res.redirect('/buyer/login'))
+    .catch(err => {
+      if(err.name == "SequelizeValidationError") {
+        let errors = err.errors.map(el => el.message)
+        res.redirect(`/buyer/daftar?errors=${errors}`)
+      } else {
+        res.send(err)
+      }
+    })
   }
 
   static buyerLogin(req, res) {
